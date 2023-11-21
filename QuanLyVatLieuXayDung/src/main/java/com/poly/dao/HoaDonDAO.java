60be0bb5105b1882b08f32535d8c392f2cf1a5d0
Thouluciferno <88932361+Thouluciferno@users.noreply.github.com>
2023-11-21 13:07:47 +0700

    Add files via upload

--- QuanLyVatLieuXayDung/src/main/java/com/poly/dao/HoaDonDAO.java
+++ QuanLyVatLieuXayDung/src/main/java/com/poly/dao/HoaDonDAO.java
@@ -15,14 +15,18 @@ import java.util.List;
  *
  * @author thien ban
  */
-public class HoaDonDAO extends QuanLyVatLieuXayDungDAO<HoaDon, String>{
+public class HoaDonDAO extends QuanLyVatLieuXayDungDAO<HoaDon, String> {
+
     private static final String INSERT_SQL = "INSERT INTO HoaDon (MaHoaDon, MaKhachHang, MaNhanVien, NgayXuat, TongTien, HinhThucThanhToan, MaVoucher) VALUES (?,?,?,?,?,?,?)";
     private static final String UPDATE_SQL = "UPDATE HoaDon SET MaKhachHang = ?, MaNhanVien = ?, NgayXuat = ?, TongTien = ?, HinhThucThanhToan = ?, MaVoucher = ? WHERE MaHoaDon = ?";
     private static final String DELETE_SQL = "DELETE FROM HoaDon WHERE MaHoaDon = ?";
     private static final String SELECT_ALL_SQL = "SELECT * FROM HoaDon";
     private static final String SELECT_BY_ID_SQL = "SELECT * FROM HoaDon WHERE MaHoaDon = ?";
     private static final String SELECT_BY_KEYWORD_SQL = "SELECT * FROM HoaDon WHERE (MaHoaDon LIKE ?)";
-    
+
+    private static final String SELECT_TOP_BY_ORDER_BY_MAHOADON_DESC_SQL
+            = "SELECT TOP 1 * FROM HoaDon ORDER BY MaHoaDon DESC";
+
     @Override
     public void insert(HoaDon entity) {
         JdbcUtil.executeUpdate(INSERT_SQL,
@@ -30,7 +34,6 @@ public class HoaDonDAO extends QuanLyVatLieuXayDungDAO<HoaDon, String>{
                 entity.getMaKhachHang(),
                 entity.getMaNhanVien(),
                 entity.getNgayXuat(),
-             
                 entity.getHinhThucThanhToan(),
                 entity.getMaVoucher());
     }
@@ -41,7 +44,6 @@ public class HoaDonDAO extends QuanLyVatLieuXayDungDAO<HoaDon, String>{
                 entity.getMaKhachHang(),
                 entity.getMaNhanVien(),
                 entity.getNgayXuat(),
-              
                 entity.getHinhThucThanhToan(),
                 entity.getMaVoucher(),
                 entity.getMaHoaDon());
@@ -81,12 +83,43 @@ public class HoaDonDAO extends QuanLyVatLieuXayDungDAO<HoaDon, String>{
                 entity.setMaKhachHang(rs.getString("MaKhachHang"));
                 entity.setMaNhanVien(rs.getString("MaNhanVien"));
                 entity.setNgayXuat(rs.getDate("NgayXuat"));
-         
+
                 entity.setHinhThucThanhToan(rs.getString("HinhThucThanhToan"));
                 entity.setMaVoucher(rs.getString("MaVoucher"));
                 list.add(entity);
             }
             rs.getStatement().getConnection().close();
+            return list;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String findTopMaHoaDonByOrderByMaHoaDonDesc() {
+        List<String> list = selectMaHoaDonBySql(SELECT_TOP_BY_ORDER_BY_MAHOADON_DESC_SQL);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    private List<String> selectMaHoaDonBySql(String sql, Object... args) {
+        List<String> list = new ArrayList<>();
+        try {
+            ResultSet rs = JdbcUtil.executeQuery(sql, args);
+            while (rs.next()) {
+                String maHoaDon = rs.getString("MaHoaDon");
+                list.add(maHoaDon);
+            }
+            // It's good practice to close ResultSet, Statement, and Connection in a finally block
+            // to ensure resources are properly released even if an exception occurs
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace(); // Handle the exception appropriately
+            }
             return list;
         } catch (SQLException e) {
             throw new RuntimeException(e);
